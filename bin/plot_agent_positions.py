e683ac5c78e5b2449ad854141d1829327064b959
Andrei Nicholson <contact@andreinicholson.com>
2012-10-24 00:12:06 -0400

    Initial agent positions plot generator

---
+++ bin/plot_agent_positions.py
@@ -0,0 +1,22 @@
+﻿#http://matplotlib.org/users/pyplot_tutorial.html
+
+import json
+import matplotlib.pyplot as pyplot
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print 'Requires path to data file'
+        sys.exit()
+
+    data_file = open(sys.argv[1]).read()
+    database = json.loads(data_file)
+
+    for agent in database['agents']:
+        pyplot.plot([position[0] for position in agent['positions']],
+                    [position[1] for position in agent['positions']])
+
+    pyplot.ylabel('y')
+    pyplot.xlabel('x')
+    pyplot.title('Agent positions')
+    pyplot.show()
